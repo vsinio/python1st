a8662a9dd4a7e7a30fe79882725195c513927bdf
Ivan <markelovv1999@yandex.ru>
2022-10-05 20:19:53 +0300

    correct task1 and do task4

--- hw4/task1.py
+++ hw4/task1.py
@@ -6,7 +6,7 @@ os.system('cls')
 
 from math import pi
 
-accuracy = input(f"Введите необходимую точность от {10**(-1)} до {10**(-10)}: \n")
+accuracy = input(f"Введите необходимую точность от {10**(-1)} до {10**(-10)} \nв формате: 0.01, 0.001, 0.001 и тд.: \n")
 dig_pi = str(pi)
 print(dig_pi[0:len(accuracy)])
 
