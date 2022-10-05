ef3a402795a0b90e1da10ee9e33b232446d523a2
Ivan <markelovv1999@yandex.ru>
2022-10-05 05:44:47 +0300

    add task1

---
+++ hw4/task1.py
@@ -0,0 +1,12 @@
+# 30). Вычислить число π c заданной точностью d
+# Пример:
+# при d = 0.001,π = 3.141 10^(-1)≤d≤10^(-10)
+import os
+os.system('cls')
+
+from math import pi
+
+accuracy = input(f"Введите необходимую точность от {10**(-1)} до {10**(-10)}: \n")
+dig_pi = str(pi)
+print(dig_pi[0:len(accuracy)])
+
