d523cbcff3ca6e33e571b98657627a8e0fd460f2
Ivan <markelovv1999@yandex.ru>
2022-10-10 18:17:20 +0300

    add some marks

--- hw4/task1.py
+++ hw4/task1.py
@@ -9,4 +9,3 @@ from math import pi
 accuracy = input(f"Введите необходимую точность от {10**(-1)} до {10**(-10)} \nв формате: 0.01, 0.001, 0.001 и тд.: \n")
 dig_pi = str(pi)
 print(dig_pi[0:len(accuracy)])
-
